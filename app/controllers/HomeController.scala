23e3415ea5fe13a1195494a0379b0ef9b91eddd5
Patryk Rudnicki <23506899+PatrykRudnicki@users.noreply.github.com>
2020-05-06 13:49:57 +0100

    b

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -14,9 +14,16 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   /**
    * Create an Action to render an HTML page.
    *
+   *
+   *
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
+   *
+   * sadas
+   *
+   *
+   *
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
